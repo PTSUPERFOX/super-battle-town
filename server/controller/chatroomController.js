8827bbf25bb362a2cf18acc1a50da942649a67f9
Fadly Kayo <f_kayo@yahoo.com>
2017-02-10 12:15:09 +0700

    Update chat box GET

--- server/controller/chatroomController.js
+++ server/controller/chatroomController.js
@@ -1,57 +1,68 @@
-let Chats = require('../models/chats')
+let Chats = require('../models/chatroom')
 
 module.exports = {
-  getItems: (req, res) => {
-    Items.find().then(function (data) {
+  getChats: (req, res) => {
+    Chats.find().then(function (data) {
       res.send(data)
     }).catch(function (err) {
-      res.json(err)
+      res.send(err)
     })
   },
-  getItem: (req, res) => {
-    Items.find({itemid: req.params.itemid}).then(function (data) {
-      res.send(data)
-    }).catch(function (err) {
-      res.json(err)
-    })
+  createChats: (req, res) => {
+    console.log('ada ', message)
+    // Chats.create({
+    //   username: req.body.username,
+    //   content: req.body.content
+    // }).then(function (data) {
+    //   res.send(data)
+    // }).catch(function (err) {
+    //   res.send(err)
+    // })
+    // let carts = JSON.parse(req.body.carts)
+    // for (let i = 0; i < carts.length; i++) {
+    //   Items.findOneAndUpdate({
+    //     _id: carts[i].id
+    //   }, {$set: {stock: (carts[i].stock) - (carts[i].qty)}}).then(function (data) {
+    //     res.json(data)
+    //   }).catch(function (err) {
+    //     res.json(err)
+    //   })
+    // }
   },
-  createItem: (req, res) => {
-    Items.create({
-      name: req.body.name,
-      itemid: req.body.itemid,
-      category: req.body.category,
-      stock: req.body.stock,
-      price: req.body.price
+  createDummy: (req, res) => {
+    Chats.create({
+      username: req.body.username,
+      content: req.body.content
     }).then(function (data) {
-      res.json(data)
+      res.send(data)
     }).catch(function (err) {
-      res.json(err)
+      res.send(err)
     })
-  },
-  deleteItem: (req, res) => {
-    Items.findOneAndRemove({itemid: req.params.itemid}).then(function (data) {
-      res.send(`Deleted Item with ItemID: ${req.params.itemid}`)
-    }).catch(function (err) {
-      res.json(err)
-    })
-  },
-  updateItem: (req, res) => {
-    Items.findOneAndUpdate({itemid: req.params.itemid}, req.body, {new: true}).then(function (data) {
-      res.json(data)
-    }).catch(function (err) {
-      res.json(err)
-    })
-  },
-  checkout: (req, res) => {
-    let carts = JSON.parse(req.body.carts)
-    for (let i = 0; i < carts.length; i++) {
-      Items.findOneAndUpdate({
-        _id: carts[i].id
-      }, {$set: {stock: (carts[i].stock) - (carts[i].qty)}}).then(function (data) {
-        res.json(data)
-      }).catch(function (err) {
-        res.json(err)
-      })
-    }
   }
+  // deleteItem: (req, res) => {
+  //   Items.findOneAndRemove({itemid: req.params.itemid}).then(function (data) {
+  //     res.send(`Deleted Item with ItemID: ${req.params.itemid}`)
+  //   }).catch(function (err) {
+  //     res.json(err)
+  //   })
+  // },
+  // updateItem: (req, res) => {
+  //   Items.findOneAndUpdate({itemid: req.params.itemid}, req.body, {new: true}).then(function (data) {
+  //     res.json(data)
+  //   }).catch(function (err) {
+  //     res.json(err)
+  //   })
+  // },
+  // checkout: (req, res) => {
+  //   let carts = JSON.parse(req.body.carts)
+  //   for (let i = 0; i < carts.length; i++) {
+  //     Items.findOneAndUpdate({
+  //       _id: carts[i].id
+  //     }, {$set: {stock: (carts[i].stock) - (carts[i].qty)}}).then(function (data) {
+  //       res.json(data)
+  //     }).catch(function (err) {
+  //       res.json(err)
+  //     })
+  //   }
+  // }
 }
