8798aec8041e067a8ff7ad8c7a27273a9b8fe24b
Syanmil <syanmil@yahoo.com>
2017-02-10 16:05:29 +0700

    temp integration

--- client/public/js/register-login.js
+++ client/public/js/register-login.js
@@ -1,4 +1,7 @@
+<<<<<<< 4c874f30c950e30561aa8bcb491356a587d0a0e3
 <<<<<<< 3ead52db586c1a7f79200613e5cd4596b34afe08
+=======
+>>>>>>> temp integration
 $('form#logMeIn').submit(function(e){
   e.preventDefault();
   let username = $("input#icon_user.validate.valid").val());
@@ -20,6 +23,7 @@ $('form#logMeIn').submit(function(e){
 
 $('form#registerMe').submit(function(e){
   e.preventDefault();
+<<<<<<< 4c874f30c950e30561aa8bcb491356a587d0a0e3
   let username = $('input#username.validate.valid').val()
   let password = $('input#password.validate.valid').val()
   let avatar = $('input.select-dropdown').val()
@@ -48,4 +52,23 @@ $('form#logMeIn').submit(function(){
   console.log($("input#icon_user.validate.valid").val());
   console.log($('input#icon_password.validate.valid').val());
 >>>>>>> resolving conflict
+=======
+  let username = $('input#username.validate.valid').val()
+  let password = $('input#password.validate.valid').val()
+  let avatar = $('input.select-dropdown').val()
+  let userData = {username: username, password: password, avatar: avatar}
+  console.log(userData);
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/api/users',
+    data: {user: userData},
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    },
+    fail: function(err){
+      console.log(err);
+    }
+  })
+>>>>>>> temp integration
 })
